203e3d8bc4b668bfbe8f63860143bb5c6d83b033
Bashar403 <133667040+Bashar403@users.noreply.github.com>
2023-11-19 14:19:05 -0500

    added both the ICoverTypeRepository and CoverTypeRepository and a using statment to the model class

---
+++ BasharBooks.DataAccess/Repository/CoverTypeRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BasharsBookstore.DataAccess.Data;
+using BasharsBookstore.Models;
+using BasharBooks.DataAccess.Repository.IRepository;
+
+namespace BasharBooks.DataAccess.Repository.IRepository
+{
+    class CoverTypeRepository
+    {
+        public class CoverTypeRepository : ICoverTypeRepository
+        {
+
+        }
+    }
+}
