827be8257d23f50560c2380294a7848b7e558c11
Bashar403 <133667040+Bashar403@users.noreply.github.com>
2023-11-19 20:20:53 -0500

    updated the js file

---
+++ BasharsBookstore/wwwroot/js/covertype.js
@@ -0,0 +1,64 @@
+﻿// wwwroot/js/covertype.js
+var dataTable;
+
+$(document).ready(function () {
+    loadDataTable();
+});
+$(document).ready(function () {
+
+    // DataTable initialization
+    var dataTable = $('#coverTypeTable').DataTable({
+        "ajax": {
+            "url": "/Admin/CoverType/GetAll",
+            "type": "GET",
+            "datatype": "json"
+        },
+        "columns": [
+            { "data": "name", "width": "60%" },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `
+                        <div class="text-center">
+                            <a href="/Admin/CoverType/Upsert/${data}" class="btn btn-success text-white" style="cursor:pointer; width:70px;">
+                                Edit
+                            </a>
+                            <a class="btn btn-danger text-white" style="cursor:pointer; width:70px;" onclick="Delete('/Admin/CoverType/Delete/${data}')">
+                                Delete
+                            </a>
+                        </div>`;
+                }, "width": "40%"
+            }
+        ],
+        "language": {
+            "emptyTable": "No data found"
+        }
+    });
+
+    // Function to delete a CoverType
+    window.Delete = function (url) {
+        swal({
+            title: "Are you sure you want to delete?",
+            text: "You will not be able to restore the data!",
+            icon: "warning",
+            buttons: true,
+            dangerMode: true
+        }).then((willDelete) => {
+            if (willDelete) {
+                // AJAX call to delete
+                $.ajax({
+                    type: "DELETE",
+                    url: url,
+                    success: function (data) {
+                        if (data.success) {
+                            toastr.success(data.message);
+                            dataTable.ajax.reload();
+                        } else {
+                            toastr.error(data.message);
+                        }
+                    }
+                });
+            }
+        });
+    };
+});
